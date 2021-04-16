3a3a3eac916c1274f3268390d7086bf574e34d55
jinyulin <jinyulin321@gmail.com>
2021-04-16 23:10:05 +0800

    create CMakeLists.txt RedBlackTree.h

---
+++ RedBlackTree.h
@@ -0,0 +1,25 @@
+/**
+ * @Author: jy1lnz
+ * @Description:
+ * @Date: 2021/4/16
+**/
+
+#ifndef ALGORITHM_READBLACKTREE_H
+#define ALGORITHM_READBLACKTREE_H
+
+struct Node
+{
+    Node()
+
+    int key;
+    int value;
+    int color;
+
+    Node * leftNode;
+    Node * rightNode;
+    Node * parentNode;
+};
+
+class Red
+
+#endif //ALGORITHM_READBLACKTREE_H
